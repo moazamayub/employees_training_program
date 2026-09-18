612219094b3b760e44e0fff3b2a20d21e6ca4792
moazamayub <moazamayub252@gmail.com>
2026-09-19 01:36:38 +0500

    pagging commit

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,12 @@ class Company < ApplicationRecord
   has_many :courses
 
   validates :name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "is_active", "created_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
